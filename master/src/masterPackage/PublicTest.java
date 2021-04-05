6ee988557668c07f648af3db797178284f5de5c8
dell <dell@DESKTOP-8ALRAUT>
2021-04-05 15:14:22 -0700

    first commit

---
+++ master/src/masterPackage/PublicTest.java
@@ -0,0 +1,12 @@
+package masterPackage;
+
+public class PublicTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("lets start");
+
+	}
+
+}
